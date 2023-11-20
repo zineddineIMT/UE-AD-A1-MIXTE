1e8ae9bd101eba1f48e63d7e5acee61b9dd5fab4
zineddineIMT <146106879+zineddineIMT@users.noreply.github.com>
2023-11-20 16:57:58 +0100

    gRPC q1-2
    ShowTimes proto + service

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -10,6 +10,16 @@ class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
 
+    def GetAllShowtimes(self, request, context):
+        for showtime in self.db:
+            yield showtime_pb2.Schedule(date=showtime["date"], movies=showtime["movies"])
+
+    def GetMoviesByDate(self, request, context):
+        for showtime in self.db:
+            if showtime["date"] == request.date:
+                return showtime_pb2.Schedule(date=showtime["date"], movies=showtime["movies"])
+        return showtime_pb2.Schedule()
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     showtime_pb2_grpc.add_ShowtimeServicer_to_server(ShowtimeServicer(), server)
